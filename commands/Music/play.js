03453cab342608ef14a4aab778a56564794a197a
Glitch (electrobeta) <none>
2020-07-10 16:48:29 +0000

    🏤🕵 Updated with Glitch

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -62,7 +62,7 @@ function play(guild, song) {
   console.log(serverQueue.songs);
 
   const dispatcher = serverQueue.connection
-    .playStream(ytdl(song.url))
+    .play(ytdl(song.url))
     .on("end", reason => {
       if (reason === "Stream is not generating quickly enough.")
         console.log("Song ended.");
