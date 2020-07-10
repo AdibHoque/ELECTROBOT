5cb6b0fc535bc588f6be7eadc86c4697820bc2dc
Glitch (electrobeta) <none>
2020-07-10 12:14:11 +0000

    👆🗼 Updated with Glitch

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -31,7 +31,7 @@ async function handleVideo(video, msg, voiceChannel, playlist = false) {
     queueConstruct.songs.push(song);
 
     try {
-      var connection = await voiceChannel.join();
+      var connection = await msg.member.voice.join();
       queueConstruct.connection = connection;
       play(msg.guild, queueConstruct.songs[0]);
     } catch (error) {
