6ef5b1035714814473a4fe6eca14c14361a2768d
Glitch (electrobeta) <none>
2020-07-19 09:39:15 +0000

    🌨🍓 Updated with Glitch

--- commands/Moderation/embed.js
+++ commands/Moderation/embed.js
@@ -6,13 +6,39 @@ module.exports = {
     category: "Moderation",
     description: "Make a embed with json format!",
     aliases: [],
-    usage: "embed <{title: \"your title\", description: \"your description\"}>",
+    usage: "embed <{\"title\": \"your title\", \"description\": \"your description\"}>",
     run: async(client, message, args) => {
         const msg = message 
+        if(!args) {
+          const a = new MessageEmbed()
+          .setTitle("ELECTRO EMBEDS")
+          .addField("Variables","title - embed title\nauthor - embed author\ndescription - embed description\nfield1title - first field title, field1text - first field text\nfield2title - second field title, field2text - second field text\ncolor - embed color\nfooter - embed footer")
+          .addField("Example","{\"title\":\"this it the title\",\"description\":\"embed description here\",\"color\":\"#ffbf00\"}")
+          .setColor("#ffbf00")
+          message.channel.send(a)
+        }
+        if(message.member.hasPermission("ADMINISTRATOR")) {
         const res = JSON.parse(args.join(" "))
         const embed = new MessageEmbed()
-        .setTitle(res.title)
-        .setDescription(res.description)
+        if(res.title) {
+          embed.setTitle(res.title)
+        }
+        if(res.description) { 
+          embed.setDescription(res.description)
+        }
+        if(res.author) {
+          embed.setAuthor(res.author)
+        }
+        if(res.color) {
+          embed.setColor(res.color)
+        }
+        if(res.footer) {
+          embed.setFooter(res.footer)
+        }
         message.channel.send(embed)
         }
+        else {
+      message.channel.send("You need the `ADMINISTRATOR` permission to use this command")
+    }
+    }
     } 
