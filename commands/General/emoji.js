694c549f88734ab5ac82245363dccf4297e171fa
Glitch (electrobeta) <none>
2020-07-20 09:48:35 +0000

    💏🍢 Updated with Glitch

--- commands/General/emoji.js
+++ commands/General/emoji.js
@@ -8,7 +8,11 @@ module.exports = {
     aliases: ["se"],
     usage: "emoji <:emoji:>",
     run: async(client, message, args) => {
-        const e = message.guild.emojis.cache.find(r => r.name.toLowerCase() === args[0].toLowerCase) || message.guild.emojis.cache.find(r => "<:"+r.name+":"+r.id+">" === args[0]) || message.guild.emojis.cache.find(r => ":"+r.name+":"=== args[0]) || message.guild.emojis.cache.get(args[0])
+        const e = message.guild.emojis.cache.find(r => r.name === args[0]) || message.guild.emojis.cache.find(r => "<:"+r.name+":"+r.id+">" === args[0]) || message.guild.emojis.cache.find(r => ":"+r.name+":"=== args[0]) || message.guild.emojis.cache.get(args[0])
+        if(!e) {
+          message.channel.send("No emojis found with that name or ID. Please make sure the name is exact as it is case sensitive!")
+          return;
+        }
         message.channel.send(e.url)
         }
     } 
