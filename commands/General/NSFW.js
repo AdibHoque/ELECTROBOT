0d8e30b7139018198669a37e3697186cbaba9196
Glitch (electrobeta) <none>
2020-08-04 13:20:25 +0000

    🙏🐺 Updated with Glitch

--- commands/General/NSFW.js
+++ commands/General/NSFW.js
@@ -6,13 +6,13 @@ const fail = "<a:ElectroFail:656772856184832025>"
 module.exports = {
     name: "NSFW",
     category: "General",
-    description: "`ass`, `pussy`, `blowjob`, `pgif`, `porn`, `anal`, `4k`, `celebrity`, `cosplay`, `gonewild`, `indian`, `asian`, `milf`, `panties`, `penis`, `pornhub`, `pornstar`, `public`, `schoolgirl`, `thighs`, `uniform`, `upskirt`, `hentai`, `hbj`, `hanal`, `hcum`, `hpussy`",
+    description: "`ass`, `pussy`, `blowjob`, `pgif`, `porn`, `vaginal`, `anal`, `4k`, `celebrity`, `cosplay`, `gonewild`, `indian`, `asian`, `milf`, `panties`, `penis`, `pornhub`, `pornstar`, `public`, `schoolgirl`, `thighs`, `uniform`, `upskirt`, `hentai`, `hbj`, `hanal`, `hcum`, `hpussy`",
     aliases: [],
     usage: "NSFW",
     run: async(client, message, args) => {
         const msg = message 
         const embed = new MessageEmbed()
-        .addField(`<:ElectroNSFWBadge:680783452563439774> NSFW COMMANDS`,"`ass`, `pussy`, `blowjob`, `pgif`, `porn`, `anal`, `4k`, `celebrity`, `cosplay`, `gonewild`, `indian`, `asian`, `milf`, `panties`, `penis`, `pornhub`, `pornstar`, `public`, `schoolgirl`, `thighs`, `uniform`, `upskirt`,`hentai`, `hbj`, `hanal`, `hcum`, `hpussy`")
+        .addField(`<:ElectroNSFWBadge:680783452563439774> NSFW COMMANDS`,"`ass`, `pussy`, `blowjob`, `pgif`, `porn`, `vaginal`, `anal`, `4k`, `celebrity`, `cosplay`, `gonewild`, `indian`, `asian`, `milf`, `panties`, `penis`, `pornhub`, `pornstar`, `public`, `schoolgirl`, `thighs`, `uniform`, `upskirt`,`hentai`, `hbj`, `hanal`, `hcum`, `hpussy`")
         .setColor("#ffbf00")
     message.channel.send(embed)
         }
