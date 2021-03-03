f1b8302f866f318e59b7b2504cee9f3c142524e3
Adib Hoque <adibhoque04@gmail.com>
2021-03-03 15:04:37 +0600

    Update loadCMD.js

--- functions/loadCMD.js
+++ functions/loadCMD.js
@@ -1,4 +1,4 @@
-const { readdirSync } = require("fs");
+const fs = require("fs");
 
 const ascii = require("ascii-table");
 
@@ -6,8 +6,8 @@ let table = new ascii("Commands");
 table.setHeading("Command", "Load status");
 
 module.exports = (client) => {
-    readdirSync("./commands/").forEach(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+    fs.readdirSync("./commands/").forEach(dir => {
+        const commands = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
 
         for (let file of commands) {
             let pull = require(`../commands/${dir}/${file}`);
