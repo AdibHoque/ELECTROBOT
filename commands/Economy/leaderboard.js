ae09a52f98ce6256e32bd23712848caa48b15239
Adib Hoque <adibhoque04@gmail.com>
2021-03-10 16:31:38 +0600

    Update leaderboard.js

--- commands/Economy/leaderboard.js
+++ commands/Economy/leaderboard.js
@@ -15,6 +15,7 @@ module.exports = {
     run: async(client, message, args) => {
       const userResult = await u.findOne({name: "users", preid: message.author.id})
         const msg = message 
+if(!args || args[0] == "1") {
         let arr = []
 	const res = await u.find( {balance: {$exists: true}}).sort([['balance', 'descending']]).limit(15)
 	
@@ -26,5 +27,19 @@ arr.push(`**${res.indexOf(r)+1}. ${r.usertag ? r.usertag : r.preid}** - $${r.bal
 	    .setDescription(arr.join("\n"))
 	    .setColor("#FFBF00")
 	    return message.channel.send(embed)	
+}}
+if(args[0] == "2") {
+let arr = []
+	const res = await u.find( {balance: {$exists: true}}).sort([['balance', 'descending']]).limit(30).slice(15)
+	
+res.forEach(r => {
+arr.push(`**${res.indexOf(r)+16}. ${r.usertag ? r.usertag : r.preid}** - $${r.balance}`)
+})
+	    const embed = new Discord.MessageEmbed()
+	    .setTitle("Leaderboard")
+	    .setDescription(arr.join("\n"))
+	    .setColor("#FFBF00")
+	    return message.channel.send(embed)	
+
 }
 }
