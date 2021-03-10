8cae8a840eb43f37a0a40179a62e66557f729a3a
Adib Hoque <adibhoque04@gmail.com>
2021-03-10 16:03:48 +0600

    Update leaderboard.js

--- commands/Economy/leaderboard.js
+++ commands/Economy/leaderboard.js
@@ -25,17 +25,6 @@ arr.push(`**${res.indexOf(r)+1}. ${r.usertag ? r.usertag : r.preid}** - $${r.bal
 	    .setTitle("Leaderboard")
 	    .setDescription(arr.join("\n"))
 	    .setColor("#FFBF00")
-	    return message.channel.send(embed)
-     /* const user1 = res[0].usertag ? res[0].usertag : res[0].preid
-      const user2 = res[1].usertag ? res[1].usertag : res[1].preid
-      const user3 = res[2].usertag ? res[2].usertag : res[2].preid
-      const user4 = res[3].usertag ? res[3].usertag : res[3].preid
-      const user5 = res[4].usertag ? res[4].usertag : res[4].preid
-      const user6 = res[5].usertag ? res[5].usertag : res[5].preid
-      const embed = new Discord.MessageEmbed()
-      .setTitle(`💰Leaderboard`)
-      .setDescription(`**${user1}** - $${res[0].balance}\n**${user2}** - $${res[1].balance}\n**${user3}** - $${res[2].balance}\n**${user4}** - $${res[3].balance}\n**${user5}** - $${res[4].balance}\n**${user6}** - $${res[5].balance}`)
-      return message.channel.send(embed);*/
-	
+	    return message.channel.send(embed)	
 }
 }
