9e76ff789b5e97263775a396c6cc25b45552cb3c
Adib Hoque <adibhoque04@gmail.com>
2021-03-18 13:27:35 +0600

    Create meme.js

---
+++ commands/Fun/meme.js
@@ -0,0 +1,21 @@
+const {MessageEmbed} = require("discord.js")
+const {RedditSimple} = require('reddit-simple')
+
+module.exports = {
+    name: "meme",
+    category: "Fun",
+    description: "Get a fresh meme from reddit!",
+    aliases: ["meirl"],
+    Usage: "meme",
+    run: async (client, message, args, nsfwembed) => { 
+
+      const post = await RedditSimple.RandomPost('me_irl');
+      const title = post[0].data.title;
+      const thumb = post[0].data.url;
+      
+      const embed = new MessageEmbed()
+        .setTitle(title)
+        .setImage(thumb)
+      message.channel.send(embed);
+  } 
+  } 
