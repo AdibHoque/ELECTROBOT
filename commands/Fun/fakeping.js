6fb99da04ee20f3a9eacb432610ede15a4efd613
Adib Hoque <adibhoque04@gmail.com>
2021-04-01 20:08:38 +0600

    Update fakeping.js

--- commands/Fun/fakeping.js
+++ commands/Fun/fakeping.js
@@ -3,20 +3,22 @@ const RichEmbed = MessageEmbed
 const Canvas = require("canvas");
 
 module.exports = {
-    name: "fakeping",
+    name: "fakepingimage",
     category: "Fun",
     description: "Generate a fake ping image!",
     aliases: [],
     usage: "whatyouseevswhatshesees [@user]",
     run: async(client, message, args) => {
-       /* const msg = message 
-        let img = message.author.avatarURL({})
-    const canvas = Canvas.createCanvas(1080, 720);
+        const msg = message 
+        const member = message.mentions.members.first() || message.member;
+        let img = member.user.avatarURL({size: 1024, format: "png", dynamic: false})
+        if(message.content.toLowerCase().includes('server')) img = message.guild.iconURL({ format: 'png', dynamic: false, size: 1024 })
+    const canvas = Canvas.createCanvas(515, 515);
     const ctx = canvas.getContext("2d");
-    const background = await Canvas.loadImage("https://cdn.discordapp.com/attachments/656517276832366595/823498077339385866/WysVSwss.jpg");
+    const background = await Canvas.loadImage(img);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    const avatar = await Canvas.loadImage(mention.user.avatarURL({size: 1024, format: "png", dynamic: false}));
-    ctx.drawImage(avatar, 359, 60, 339, 420);
+    const avatar = await Canvas.loadImage(mention.user.avatarURL("https://cdn.discordapp.com/attachments/656517276832366595/827162267668971570/FakePing.png")
+    ctx.drawImage(avatar, 0, 0, canvas.width, canvas.height);
     msg.channel.send({
       files: [
         {
@@ -24,6 +26,6 @@ module.exports = {
           name: "MadeWithElectro.png"
         }
       ]
-    });*/
+    });
         }
     } 
