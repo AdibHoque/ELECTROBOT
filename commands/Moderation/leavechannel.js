d2e16159fbef4256e4d9bc5e68eef670f51b563a
Adib Hoque <adibhoque04@gmail.com>
2021-04-09 22:47:21 +0600

    Update leavechannel.js

--- commands/Moderation/leavechannel.js
+++ commands/Moderation/leavechannel.js
@@ -1,46 +1,54 @@
 const {Discord, MessageEmbed} = require("discord.js");
 const RichEmbed = MessageEmbed
 const mongoose = require("mongoose");
-const check = "<a:ElectroCheck:709464171825201315>"
-const fail = "<:ElectroCross:737678614681878580>"
+const guildData = require("./../../Mongodb/guild")
 
 module.exports = {
-    name: "setlogs",
+    name: "leavechannel",
     category: "Moderation",
-    description: "Set the loggin channel for the server!",
-    aliases: ["setuplogs","setuplogchannel","logchannel"],
-    usage: "setlogs <#channel>",
-    run: async (client, message, args) => {
+    description: "Set the leave message channel!",
+    aliases: ["leave-message-channel", "setleavechannel", "setlc"],
+    usage: "leavechannel <#channel>",
+    run: async(client, message, args) => { 
         const msg = message 
+
 if(!message.member.hasPermission("ADMINISTRATOR")) {
-  return message.channel.send("YOU NEED TO PERMISSION `ADMINISTRATOT` PERMISSION TO USE THIS COMMAND!")
+  return message.channel.send("YOU NEED THE `ADMINISTRATOR` PERMISSION TO USE THIS COMMAND!")
 }
-const pre = require("./../../Mongodb/logchannel")
-const lc = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
 
-mongoose.connect("mongodb+srv://ELECTRO:electrobot6969@electro-jbqon.mongodb.net/Guilds?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
-/*const res = await pre.findOne({name: "prefix", preid: message.guild.id})
-  let prefix = res ? res.prefix : "didnt set";
-  message.channel.send(prefix)*/
+let cArgs = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
+
+    if (!cArgs)
+      return message.channel.send(
+        "You must specify a valid id for the leave message channel!"
+      ); 
 
 
-pre.findOne({name: "logchannel", preid: message.guild.id}).then(result => {
-        let duck = new pre({
+guildData.findOne({name: "guild", preid: message.guild.id}).then(result => {
+      
+        if(!result) {
+let duck = new guildData({
             _id: new mongoose.Types.ObjectId(),
-            name: "logchannel",
+            name: "guild",
             preid: message.guild.id,
-            prefix: lc.id
-          })
-        const embed = new MessageEmbed()
-        .setTitle("Logging Channel Changed")
-        .setDescription(`${check} **| The new logchannel for the server is ${lc}.**`)
-        .setColor("#ffbf00")
-        message.channel.send(embed);
-        if(!result || result == []) {
+            leavechannel: cArgs
+          }) 
  duck.save().catch(console.error);
-        }else{
-          pre.deleteOne({name: "logchannel", preid: message.guild.id}).catch(console.error)          
-          duck.save().catch(console.error)
+const embed = new MessageEmbed()
+        .setTitle("Leave Channel Set")
+        .setDescription(`Goodbye Text Will Be Deliverd To <#${cArgs}> Everytime Someone Leaves.`)
+        .setColor("#ffbf00")
+       return message.channel.send(embed); 
+        }
+      else{
+          result.leavechannel = cArgs
+          result.save().catch(console.error);
+
+          const embed = new MessageEmbed()
+        .setTitle("Join Channel Set")
+        .setDescription(`Goodbye Text Will Be Deliverd To <#${cArgs}> Everytime Someone Leaves.`)
+        .setColor("#ffbf00")
+        return message.channel.send(embed); 
         }
       })
-        }}
+        }}  
