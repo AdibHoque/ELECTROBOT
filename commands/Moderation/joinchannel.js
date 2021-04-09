93bf98e5183f1ba27865d53cd5180a817dd129f9
Adib Hoque <adibhoque04@gmail.com>
2021-04-09 22:34:05 +0600

    Update joinchannel.js

--- commands/Moderation/joinchannel.js
+++ commands/Moderation/joinchannel.js
@@ -1,6 +1,7 @@
 const {Discord, MessageEmbed} = require("discord.js");
 const RichEmbed = MessageEmbed
-const db = require('quick.db')
+const mongoose = require("mongoose");
+const guildData = require("./../../Mongodb/guild")
 
 module.exports = {
     name: "joinchannel",
@@ -8,34 +9,45 @@ module.exports = {
     description: "Set the join message channel!",
     aliases: ["join-message-channel", "setwelcomer", "setjc"],
     usage: "joinchannel <#channel>",
-    run: async(client, message, args) => {
-        let permission = message.member.hasPermission("ADMINISTRATOR");
+    run: async(client, message, args) => { 
+        const msg = message 
 
-    if (!permission)
-      return message.channel.send(
-        "You are missing the permission `ADMINISTRATOR`"
-      );
+if(!message.member.hasPermission("ADMINISTRATOR")) {
+  return message.channel.send("YOU NEED THE `ADMINISTRATOR` PERMISSION TO USE THIS COMMAND!")
+}
 
-    let cArgs = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
+let cArgs = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
 
     if (!cArgs)
       return message.channel.send(
         "You must specify a valid id for the welcome channel!"
-      );
+      ); 
 
-    try {
-      message.guild.channels.cache.get(cArgs.id).send("Welcome channel set!");
 
-      await db.set(`jc${message.channel.guild.id}`, cArgs.id);
-
-      message.channel.send(
-        "You have successfully set the welcome channel to <#" + cArgs.id + ">"
-      );
-      return;
-    } catch (e) {
-      return message.channel.send(
-        "Error: missing permissions or channel doesn't exist"
-      );
-    }
+guildData.findOne({name: "guild", preid: message.guild.id}).then(result => {
+      
+        if(!result) {
+let duck = new guildData({
+            _id: new mongoose.Types.ObjectId(),
+            name: "guild",
+            preid: message.guild.id,
+            joinchannel: cArgs
+          }) 
+ duck.save().catch(console.error);
+const embed = new MessageEmbed()
+        .setTitle("Join Channel Set")
+        .setDescription(`Greetings Text Will Be Deliverd To <#${cArgs}> Everytime Someone Joins.`)
+        .setColor("#ffbf00")
+       return message.channel.send(embed); 
         }
-    } 
+      else{
+          result.joinchannel = cArgs
+          await result.save()
+          const embed = new MessageEmbed()
+        .setTitle("Join Channel Set")
+        .setDescription(`Greetings Text Will Be Deliverd To <#${cArgs}> Everytime Someone Joins.`)
+        .setColor("#ffbf00")
+        return message.channel.send(embed); 
+        }
+      })
+        }} 
