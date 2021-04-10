1d3157777b32e24769cdf02ea9c8c8cf403c9bc0
Adib Hoque <adibhoque04@gmail.com>
2021-04-10 20:15:44 +0600

    Create tictactoe.js

---
+++ commands/Fun/tictactoe.js
@@ -0,0 +1,25 @@
+const {Discord, MessageEmbed} = require("discord.js");
+const db = require("quick.db")
+const check = "<a:ElectroCheck:709464171825201315>"
+const fail = "<a:ElectroFail:656772856184832025>"
+
+module.exports = {
+    name: "tictactoe",
+    category: "Fun",
+    description: "Play tictactoe",
+    aliases: ["ttt"],
+    usage: "ttt",
+    run: async(client, message, args) => {
+    return; 
+        let a = '🇦'
+        let b = '🇧'
+        let c = '🇨'
+        let d = '🇩'
+        let e = '🇪'
+        let f = '🇫'
+        let g = '🇬'
+        let h = '🇭'
+        let i = '🇮'
+        
+        }
+    }  
