679be7bbb63056b148e491d54fcda69609cf242b
Adib Hoque <adibhoque04@gmail.com>
2021-04-19 22:05:18 +0600

    Update tictactoe.js

--- commands/Fun/tictactoe.js
+++ commands/Fun/tictactoe.js
@@ -12,8 +12,24 @@ module.exports = {
     usage: "tictactoe <@User>",
     run: async(client, message, args) => {
       const member = message.mentions.members.first()
-      if(!member) return message.channel.send(`${fail} **PLEASE MENTION A VALID USER TO PLAY TICTACTOE WITH!**`);
-      //if(member.user.id == message.author.id) return message.channel.send(`${fail} **YOU CAN\'T PLAY TICTACTOE WITH YOURSELF!**`)
+      if(!member) {
+message.channel.send(`**Play a TicTacToe match**\nArrange 3 of your tile in a line within the board!\n\n☑  ${message.author.username}
+Needs at least 1 more player\n\nType \`${prefix}play\` to play against them!`)
+message.channel.awaitMessages(m => !m.author.bot && m.content == prefix+'join', {
+		max: 1,
+		time: 30000,
+		errors: ['time'],
+	}).then(async collected => {
+		if (!collected.first()) return;
+		if (collected.first().content.toLowerCase() == prefix+'join') {
+return new tictactoe({
+    message: collected.first((,
+    player_two: message.member,
+}); 
+}
+
+})
+}
 
 new tictactoe({
     message: message,
