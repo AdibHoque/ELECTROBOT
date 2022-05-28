2d6829ffb0f473d47cafc500d8c7281a140f5318
Adib Hoque <adibhoque04@gmail.com>
2022-05-28 22:21:25 +0600

    Update index.js

--- index.js
+++ index.js
@@ -10,58 +10,6 @@ app.listen(process.env.PORT);
 /*setInterval(() => {
   http.get(`https://electr.herokuapp.com/`);
 }, 280000);*/
-setInterval(() => {
-  const ty = [
-    'hentai',
-    'gonewild',
-    'thigh',
-    'pussy',
-    'ass',
-    '4k',
-    'pgif', 
-    'anal',
-    'hanal'
-    ]
-  var types = ty[Math.round(Math.random() * (ty.length - 1))];
-  let options = {
-url: "https://nekobot.xyz/api/image?type="+types,
-json: true
-}
-get(options).then(async body => {
-const p = body.message.replace("https://cdn.nekobot.xyz/","https://electro-bot.glitch.me/api/img/")
-const c1 = client.channels.cache.get("738741744602054657")
-const c2 = client.channels.cache.get("738787404931923989")
-if(c1) c1.send(p)
-if(c2) c2.send(p)
-}
-    )
-}, 30000);
-
-setInterval(() => {
-  const ty = [
-    'cum_jpg',
-    'pussy_jpg',
-    'pwankg',
-    'tits',
-    'boobs',
-    'bj',
-    'cum', 
-    'blowjob'
-    ]
-  var types = ty[Math.round(Math.random() * (ty.length - 1))];
-  let options = {
-url: "https://nekos.life/api/v2/img/"+types,
-json: true
-}
-get(options).then(async body => {
-const p = body.url.replace("https://cdn.nekos.life/","https://electro-bot.glitch.me/api/img/")
-const c1 = client.channels.cache.get("738741744602054657")
-const c2 = client.channels.cache.get("738787404931923989")
-if(c1) c1.send(p)
-if(c2) c2.send(p)
-}
-    )
-}, 60000);
 
 const { Discord, MessageEmbed, Util} = require("discord.js");
 const Client = require("./Classes/Client");
